70d3377fe9a72174f321170e6a9ce6daf6d537b8
akmal subakti <akmalsubakti528@gmail.com>
2021-12-13 19:38:53 +0700

    update library css bootstrap

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,9 +41,9 @@
 <link href="{{ url('/') }}/assets/lib/jquery-switchbutton/jquery.switchButton.css" rel="stylesheet">
 <link href="{{ url('/') }}/assets/lib/rickshaw/rickshaw.min.css" rel="stylesheet">
 <link href="{{ url('/') }}/assets/lib/chartist/chartist.css" rel="stylesheet">
-<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
-<link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-{{-- <link href="{{ url('/') }}/assets/lib/datatables/jquery.dataTables.css" rel="stylesheet"> --}}
+{{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet">
+<link href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css" rel="stylesheet"> --}}
+<link href="{{ url('/') }}/assets/lib/datatables/jquery.dataTables.css" rel="stylesheet">
 <link href="{{ url('/') }}/assets/lib/select2/css/select2.min.css" rel="stylesheet">
 
 <!-- Bracket CSS -->
