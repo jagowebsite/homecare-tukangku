b9a1cde4b321e7e1b57a753c460b89c1286df31f
akmal subakti <akmalsubakti528@gmail.com>
2021-12-16 11:38:12 +0700

    add notification

--- resources/views/pages/setting/notification/index.blade.php
+++ resources/views/pages/setting/notification/index.blade.php
@@ -30,7 +30,7 @@
                     <div class="col-12">
                         <a
                             href="{{ @$item->data['action']}}">
-                            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            <div class="alert {{@$item->read_at ? 'alert-success' : 'alert-light'}}  alert-dismissible fade show" role="alert">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                   </button>
